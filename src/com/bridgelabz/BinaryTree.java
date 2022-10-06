da36a77a9b5481d65f0adf862b00d07a4f4fcec5
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-10-06 17:48:09 +0530

    Added UC2 cheking size of BST

--- src/com/bridgelabz/BinaryTree.java
+++ src/com/bridgelabz/BinaryTree.java
@@ -2,43 +2,44 @@ package com.bridgelabz;
 
 public class BinaryTree 
 {
-	public Node root;
-    public BinaryTree(){
+	  //Represent the root of binary tree
+    public Node root;
+    public BinaryTree()
+    {
         root = null;
     }
 
-    public Node insert(Node currentNode,int data)
-    {
+    public Node insert(Node currentNode,int data){
         if ( currentNode == null){
             Node newNode = new Node(data);
             currentNode = newNode;
             return currentNode;
-        }
-        else if (data <= currentNode.data)
-        {
+        }else if (data <= currentNode.data){
             currentNode.left = insert(currentNode.left, data);
-            return currentNode;
-        }else 
-        {
+           return currentNode;
+        }else{
             currentNode.right = insert(currentNode.right, data);
             return currentNode;
         }
     }
 
     public void inOrderTraversal(Node root){
-        if (root == null)
-        {
+        if (root == null){
             return;
         }
-        else 
-        {
+        else {
             inOrderTraversal(root.left);
             System.out.print(root.data +" ");
             inOrderTraversal(root.right);
         }
     }
-    public static void main(String[] args) 
-    {
+    public int size(Node root){
+        if ( root == null){
+            return 0;
+        }
+        return 1 + size(root.left) + size(root.right);
+    }
+    public static void main(String[] args) {
         System.out.println("Binary search tree problem");
 
         BinaryTree binaryTree = new BinaryTree();
@@ -46,8 +47,19 @@ public class BinaryTree
         binaryTree.root = binaryTree.insert( binaryTree.root,56);
         binaryTree.insert(binaryTree.root, 30);
         binaryTree.insert(binaryTree.root, 70);
-        
-        binaryTree.inOrderTraversal(binaryTree.root);
-    }
+        binaryTree.insert(binaryTree.root, 22);
+        binaryTree.insert(binaryTree.root, 40);
+        binaryTree.insert(binaryTree.root, 60);
+        binaryTree.insert(binaryTree.root, 95);
+        binaryTree.insert(binaryTree.root, 11);
+        binaryTree.insert(binaryTree.root, 65);
+        binaryTree.insert(binaryTree.root, 3);
+        binaryTree.insert(binaryTree.root, 16);
+        binaryTree.insert(binaryTree.root, 63);
+        binaryTree.insert(binaryTree.root, 67);
 
+        binaryTree.inOrderTraversal(binaryTree.root);
+
+        System.out.println("\nSize : "+ binaryTree.size(binaryTree.root));
+    }
 }
